5b23be29e458cbde8d97e769cfe0d6705afeb9c1
justan0therdave <justanotherdavemartin@gmail.com>
2016-05-03 11:27:17 +0100

    added vertical rythym

--- page.php
+++ page.php
@@ -12,12 +12,6 @@
 
 								<header class="article-header">
 
-									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-
-									<p class="byline vcard">
-										<?php printf( __( 'Posted', 'babe').' <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> '.__( 'by',  'babe').' <span class="author">%3$s</span>', get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p>
-
 								</header> <?php // end article header ?>
 
 								<section class="entry-content cf" itemprop="articleBody">
